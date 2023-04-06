6ebd89a19f8fb00cc0507efb39c0c86b78c07912
neffjulian <julian.neff@inf.ethz.ch>
2023-04-06 08:57:59 +0200

    Added requirements.txt

--- images/sentinel_2/s2_setup.py
+++ images/sentinel_2/s2_setup.py
@@ -8,6 +8,6 @@ def create_data(name: str, time_of_interest: str):
     preprocess_data(name)
 
 if __name__ == "__main__":
-    create_data("22_march", "2022-03-01/2022-05-31")    
-    create_data("22_may", "2022-05-01/2022-05-31")    
-    create_data("22_july", "2022-07-01/2022-07-31")
+    create_data("22_november", "2022-11-01/2022-11-30")    
+    # create_data("22_may", "2022-05-01/2022-05-31")    
+    # create_data("22_july", "2022-07-01/2022-07-31")
