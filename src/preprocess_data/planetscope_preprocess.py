cc85555f01d2fcf4f76ea73b091968c1b0c81361
neffjulian <julian.neff@inf.ethz.ch>
2023-05-07 15:45:09 +0200

    Added resize, normalizeation

--- src/preprocess_data/planetscope_preprocess.py
+++ src/preprocess_data/planetscope_preprocess.py
@@ -2,20 +2,106 @@ import os
 import argparse
 from pathlib import Path
 
+import shutil
+import rasterio
+import numpy as np
+import cv2
+
 from utils import (
     MONTHS,
-    DATA_DIR
+    DATA_DIR,
+    numpy_resize_normalize,
+    copy_metadata
 )
 
-def preprocess_planetscope_data(year: str, month: str) -> None:
-    
+def check_sentinel_indices(year: str, month: str) -> list[str]:
+    sentinel_dir = DATA_DIR.joinpath("raw", "sentinel", year, f"{MONTHS[month]}_{month}", "metadata")
+    indices = [f.name[:4] for f in sentinel_dir.iterdir() if f.name[:4].isdigit()]
+    print(f"Found {len(indices)} Sentinel files for {month} {year}")
+    return indices
+
+def check_planetscope_indices(source_dir):
+    for file in source_dir.iterdir():
+        print(file.name, type(file.name))
+
+
+def copy_planetscope_data(year: str, month: str) -> None:
     source_dir = DATA_DIR.joinpath("raw", "planetscope", year, f"{MONTHS[month]}_{month}", "data")
-    target_dir = DATA_DIR.joinpath("preprocessed", "planetscope", year, f"{MONTHS[month]}_{month}", "data")
-    target_dir.mkdir(parents=True, exist_ok=True)
+    target_dir = DATA_DIR.joinpath("raw_filtered", "planetscope", year, f"{MONTHS[month]}_{month}")
     
+    data_dir = target_dir.joinpath("data")
+    data_dir.mkdir(parents=True, exist_ok=True)
+    metadata_dir = target_dir.joinpath("metadata")
+    metadata_dir.mkdir(parents=True, exist_ok=True)
+
     for root, dirs, files in os.walk(source_dir):
-        for filename in files:
-            print(root, filename)
+        curr_index = Path(root).name
+        if not curr_index.isdigit():
+            continue
+
+        curr_dir = Path(root).joinpath(dirs[0])
+
+        for file in curr_dir.iterdir():
+            if "Analytic" in file.name:
+                if file.name.endswith(".tif"):
+                    shutil.copy(src=file,
+                        dst=data_dir.joinpath(f"{curr_index}.tif"),
+                        follow_symlinks=False
+                    )
+                elif file.name.endswith(".xml"):
+                    shutil.copy(
+                        src=file,
+                        dst=metadata_dir.joinpath(f"{curr_index}.xml"),
+                        follow_symlinks=False
+                    )
+                else:
+                    raise Exception("Error with file: ", file, " should be either a .tif file or .xml")
+                
+def crop_data_and_save_as_np(src: Path, dst: Path, plot_loc: Path):
+    with rasterio.open(src, 'r') as data:
+        b_02 = data.read(2)
+        b_04 = data.read(4)
+        b_06 = data.read(6)
+        b_07 = data.read(7)
+        b_08 = data.read(8)
+
+    # output_dim = (546, 546)
+    output_dim = (384, 384)
+    b_02_resized = numpy_resize_normalize(b_02, output_dim)
+    b_04_resized = numpy_resize_normalize(b_04, output_dim)
+    b_06_resized = numpy_resize_normalize(b_06, output_dim)
+    b_07_resized = numpy_resize_normalize(b_07, output_dim)
+    b_08_resized = numpy_resize_normalize(b_08, output_dim)
+
+    np.savez(dst,
+        Blue = b_02_resized,
+        Green = b_04_resized,
+        Red = b_06_resized,
+        RedEdge = b_07_resized,
+        NIR = b_08_resized
+    )
+
+    rgb = np.dstack((b_02_resized, b_04_resized, b_06_resized))
+    cv2.imwrite(plot_loc.as_posix(), rgb)
+                    
+def preprocess_planetscope_data(year, month):
+    source_dir = DATA_DIR.joinpath("raw_filtered", "planetscope", year, f"{MONTHS[month]}_{month}", "data")
+    target_dir = DATA_DIR.joinpath("processed", "planetscope", year, f"{MONTHS[month]}_{month}", "data")
+    plot_dir = DATA_DIR.joinpath("processed", "planetscope", year, f"{MONTHS[month]}_{month}", "plot")
+
+    target_dir.mkdir(exist_ok=True, parents=True)
+    plot_dir.mkdir(exist_ok=True, parents=True)
+
+    for file in source_dir.iterdir():
+        if not file.name[:4].isdigit():
+            raise Exception("Invalid file found in folder", file)
+        
+        curr_target_filename = target_dir.joinpath(f"{file.name[:4]}.npz")
+        curr_target_plotname = plot_dir.joinpath(f"{file.name[:4]}.png")
+
+        crop_data_and_save_as_np(file, curr_target_filename, curr_target_plotname)
+    
+    copy_metadata("planetscope", year, month)
 
 def main(year: str, month: str) -> None:
     if not (2017 <= int(year) <= 2022):
@@ -24,6 +110,7 @@ def main(year: str, month: str) -> None:
     if month not in MONTHS:
         raise ValueError(f"Month invalid ('{month}'). Use one out of {list(MONTHS)}.")
     
+    # copy_planetscope_data(year, month)
     preprocess_planetscope_data(year, month)
 
 if __name__ == "__main__":
--- src/preprocess_data/sentinel_preprocess.py
+++ src/preprocess_data/sentinel_preprocess.py
@@ -2,19 +2,79 @@ import os
 import argparse
 from pathlib import Path
 
-from distutils.dir_util import copy_tree
+import numpy as np
+import rasterio
+import cv2
+
+from shutil import copytree
 
 from utils import (
     MONTHS,
-    DATA_DIR
+    DATA_DIR,
+    numpy_resize_normalize,
+    copy_metadata
 )
 
-def preprocess_sentinel_data(year: str, month: str) -> None:
-    source_dir = DATA_DIR.joinpath("raw", "sentinel", year, f"{MONTHS[month]}_{month}", "data")
-    target_dir = DATA_DIR.joinpath("preprocessed", "sentinel", year, f"{MONTHS[month]}_{month}", "data")
-    # target_dir.mkdir(parents=True, exist_ok=True)
-    copy_tree(source_dir.as_posix(), target_dir.as_posix())
+def copy_sentinel_data(year: str, month: str) -> None:
+    source_dir = DATA_DIR.joinpath("raw", "sentinel", year, f"{MONTHS[month]}_{month}")
+    target_dir = DATA_DIR.joinpath("raw_filtered", "sentinel", year, f"{MONTHS[month]}_{month}")
+    
+    copytree(source_dir, target_dir, dirs_exist_ok=True)
 
+def crop_data_and_save_as_np(src_10m: Path, src_20m: Path, dst_loc: Path, plot_loc: Path):
+    with rasterio.open(src_10m, 'r') as data:
+        b_02 = data.read(1)
+        b_03 = data.read(2)
+        b_04 = data.read(3)
+
+        if min(b_02.shape, b_03.shape, b_04.shape) < (200, 200):
+            raise Exception("Data with invalid shape", src_10m.name)
+    
+    output_dim_10m = (196, 196)
+    b_02_resized = numpy_resize_normalize(b_02, output_dim_10m)
+    b_03_resized = numpy_resize_normalize(b_03, output_dim_10m)
+    b_04_resized = numpy_resize_normalize(b_04, output_dim_10m)
+    
+    with rasterio.open(src_20m, 'r') as data:
+        b_05 = data.read(1)
+        b_8A = data.read(2)
+
+        if min(b_05.shape, b_8A.shape) < (100, 100):
+            raise Exception("Data with invalid shape", src_20m.name)
+    
+    output_dim_20m = (96, 96)
+    b_05_resized = numpy_resize_normalize(b_05, output_dim_20m)
+    b_8A_resized = numpy_resize_normalize(b_8A, output_dim_20m)
+
+    np.savez(dst_loc,
+        Blue = b_02_resized,
+        Green = b_03_resized,
+        Red = b_04_resized,
+        RedEdge = b_05_resized,
+        NIR = b_8A_resized
+    )        
+
+    rgb = np.dstack((b_02_resized, b_03_resized, b_04_resized))
+    cv2.imwrite(plot_loc.as_posix(), rgb)
+
+def preprocess_sentinel_data(year: str, month: str) -> None:
+    source_dir = DATA_DIR.joinpath("raw_filtered", "sentinel", year, f"{MONTHS[month]}_{month}", "data")
+    target_dir = DATA_DIR.joinpath("processed", "sentinel", year, f"{MONTHS[month]}_{month}", "data")
+    plot_dir = DATA_DIR.joinpath("processed", "sentinel", year, f"{MONTHS[month]}_{month}", "plot")
+
+    target_dir.mkdir(parents=True, exist_ok=True)
+    plot_dir.mkdir(parents=True, exist_ok=True)
+
+    for file in source_dir.iterdir():
+        if file.name.endswith("10m.tif"):
+            file_20m = Path(file.as_posix().replace("10m", "20m"))
+            curr_target_filename = target_dir.joinpath(f"{file.name[:4]}.npz")
+            curr_target_plotname = plot_dir.joinpath(f"{file.name[:4]}.png")
+            crop_data_and_save_as_np(file, file_20m, curr_target_filename, curr_target_plotname)
+    
+    copy_metadata("sentinel", year, month)
+
+    
 
 def main(year: str, month: str) -> None:
     if not (2017 <= int(year) <= 2022):
@@ -23,6 +83,7 @@ def main(year: str, month: str) -> None:
     if month not in MONTHS:
         raise ValueError(f"Month invalid ('{month}'). Use one out of {list(MONTHS)}.")
     
+    # copy_sentinel_data(year, month)
     preprocess_sentinel_data(year, month)
 
 if __name__ == "__main__":
