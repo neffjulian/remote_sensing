771139431ae0e712c3af8d6d444a4c359e049f17
neffjulian <julian.neff@inf.ethz.ch>
2023-06-20 20:18:01 +0200

    New download.py and preprocess.py added histogram matching

--- src/download_data.py
+++ src/download_data.py
@@ -3,45 +3,20 @@ from pathlib import Path
 import wget
 import py7zr
 
-FILTERED = Path(__file__).parent.parent.joinpath("data", "filtered")
-FILTERED_IN_SITU = FILTERED.joinpath("in_situ")
-
-FILTERED.mkdir(exist_ok=True, parents=True)
-FILTERED_IN_SITU.mkdir(exist_ok=True, parents=True)
+DATA_DIR = Path(__file__).parent.parent.joinpath("data")
 
 def download_zip_files():
-    print("Start downloading Sentinel-2 data...")
-    sentinel_url = "https://polybox.ethz.ch/index.php/s/In45d1I7zT6lLxb/download"
-    sentinel_in_situ_url = "https://polybox.ethz.ch/index.php/s/MXe8uWICvYRFG7p/download"
-    wget.download(sentinel_url,out=str(FILTERED))
-    wget.download(sentinel_in_situ_url,out=str(FILTERED_IN_SITU))
-    print("\n...finished!")
+    url = "https://polybox.ethz.ch/index.php/s/e5yrmHcejuCo0WO/download"
+    wget.download(url,out=str(DATA_DIR))
 
-    print("Start downloading PlanetScope data...")
-    planetscope_url = "https://polybox.ethz.ch/index.php/s/zQYzJY4kLNvaxGA/download"
-    planetscope_in_situ_url = "https://polybox.ethz.ch/index.php/s/1Lfok4hQVJDUycK/download"
-
-    wget.download(planetscope_url,out=str(FILTERED))
-    wget.download(planetscope_in_situ_url,out=str(FILTERED_IN_SITU))
-
-    print("\n...finished!")
-
-def unzip_and_delete(satellite: str):
-    print(f"Unzipping {satellite} files...")
-    data_zip = FILTERED.joinpath(f"{satellite}.7z")
-    in_situ_zip = FILTERED_IN_SITU.joinpath(f"{satellite}_in_situ.7z")
+def unzip_and_delete():
+    data_zip = DATA_DIR.joinpath("filtered.7z")
 
     with py7zr.SevenZipFile(data_zip) as z:
-        z.extractall(FILTERED)
-
-    with py7zr.SevenZipFile(in_situ_zip) as z:
-        z.extractall(FILTERED_IN_SITU)
+        z.extractall(DATA_DIR)
 
     data_zip.unlink()
-    in_situ_zip.unlink()
-    print("\n...finished!")
 
 if __name__ == "__main__":
     download_zip_files()
-    unzip_and_delete("sentinel")
-    unzip_and_delete("planetscope")
+    unzip_and_delete()
--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from math import log10, sqrt
 
 import numpy as np
 import cv2
@@ -7,6 +8,8 @@ from eodal.core.raster import RasterCollection
 from skimage.exposure import match_histograms
 
 from matplotlib import pyplot as plt
+from skimage.metrics import structural_similarity as ssim
+
 
 MONTHS = {"jan": "01", "feb": "02", "mar": "03", "apr": "04", 
           "may": "05", "jun": "06", "jul": "07", "aug": "08", 
@@ -32,7 +35,9 @@ def remove_files(foldername: str, files_to_keep: list[str], in_situ: bool) -> No
     else:
         dir = DATA_DIR.joinpath("processed", foldername)
 
-    [file.unlink() for file in dir.iterdir() if file.name not in files_to_keep]
+    for file in dir.iterdir():
+        if not file.name in files_to_keep:
+            file.unlink()
 
 def remove_unused_images(in_situ: bool = False) -> None:
     """Remove unused images from the processed folders."""
@@ -48,9 +53,9 @@ def remove_unused_images(in_situ: bool = False) -> None:
     else:
         files = [[file.name for file in DATA_DIR.joinpath("processed", folder).iterdir()] for folder in FOLDERS.keys()]
     
-    assert all(files[0] == files[i] for i in range(1, len(files)))
+    assert all(files[0] == files[i] for i in range(1, len(files))), "Error in original Sentinel-2 Files"
 
-def preprocess_file(src_data: Path, tar_dir: Path, rotate: bool = False) -> None:
+def preprocess_file(src_data: Path, tar_dir: Path, in_situ: bool, rotate: bool = False) -> None:
     """Preprocess a single file and save the tiles."""
     file_index = src_data.name[:4]
     month = src_data.parent.parent.name[:2]
@@ -58,10 +63,15 @@ def preprocess_file(src_data: Path, tar_dir: Path, rotate: bool = False) -> None
 
     raster = RasterCollection.from_multi_band_raster(src_data)
     lai = cv2.resize(raster["lai"].values, OUT_DIM, interpolation=cv2.INTER_CUBIC)
-    lai_processed = np.clip(np.nan_to_num(lai), 0, 8)
-    tiles = create_tiles(lai_processed)
 
+    if np.isnan(lai).sum() / lai.size > 0.1:
+        return
+    
+    tiles = create_tiles(lai, in_situ)
     for tile_index, tile in enumerate(tiles):
+        if tile is None:
+            continue
+
         if rotate:
             for i in range(4):
                 number = tile_index * 4 + i
@@ -77,28 +87,39 @@ def is_outlier(src: Path, band: int) -> bool:
     raster = RasterCollection.from_multi_band_raster(src)
     data = raster['lai'].values
     x, y = data.shape
+    return min(x, y) < band_size[band]
 
-    if min(x, y) < band_size[band]:
-        return True
-
-    percentage_ones = (np.sum(data == 1) / data.size) * 100
-    percentage_zeros = (np.sum(data == 0) / data.size) * 100
-
-    if max(percentage_ones, percentage_zeros) > 15:
-        return True
-
-    return False
-
-def create_tiles(data: np.ndarray, tiles: int = 2):
+def create_tiles(data: np.ndarray, in_situ: bool, tiles: int = 4):
     """Create tiles from the given data."""
     sub_data = []
     arr_x, arr_y = data.shape
-    x, y = int(arr_x / tiles), int(arr_y / tiles)
-
+    x, y = arr_x // tiles, arr_y // tiles
     for i in range(tiles):
         for j in range(tiles):
-            sub_data.append(data[(i * x):((i + 1) * x), (j * y):((j + 1) * y)])
+            x_start, x_end = i * x, (i + 1) * x
+            y_start, y_end = j * y, (j + 1) * y
 
+            if not in_situ:
+                margin = 40
+                if i == 0:
+                    x_start += margin
+                    x_end += margin
+                if i == tiles - 1:
+                    x_start -= margin
+                    x_end -= margin
+                if j == 0:
+                    y_start += margin
+                    y_end += margin
+                if j == tiles - 1:
+                    y_start -= margin
+                    y_end -= margin
+
+            tile = data[x_start:x_end, y_start:y_end]
+
+            if np.isnan(tile).sum() / tile.size > 0.01 and not in_situ:
+                sub_data.append(None)
+            else:
+                sub_data.append(np.clip(np.nan_to_num(tile), 0., 8.))
     return sub_data
 
 def preprocess_folder(folder: Path, band: str, in_situ: bool) -> None:
@@ -118,7 +139,7 @@ def preprocess_folder(folder: Path, band: str, in_situ: bool) -> None:
     for file in folder.iterdir():
         if file.name.endswith(file_ending):
             if not is_outlier(file, FOLDERS[band]):
-                preprocess_file(file, target_dir)
+                preprocess_file(file, target_dir, in_situ)
 
 def preprocess(satellite: str, in_situ: bool):
     if in_situ:
@@ -148,7 +169,7 @@ def rename_in_situ_data():
                 new_name = file.replace(file[8:12], index_to_field[file[8:12]])
                 path.rename(Path(root).joinpath(new_name))
 
-def check_and_remove_outliers(threshold:float = .85):
+def check_and_remove_outliers(threshold:float = .75):
     print(f"Checking for outliers which have more than {int((1-threshold)*100)}% of zero entries")
     folder_names = ["4b", "8b", "10m", "20m"]
     folders = [DATA_DIR.joinpath("processed", folder_name) for folder_name in folder_names]
@@ -159,7 +180,7 @@ def check_and_remove_outliers(threshold:float = .85):
 
     to_remove = list(set(to_remove))
     number_of_files = len([file for file in folders[0].iterdir()])
-    print(f"Percentage of outliers {int(len(to_remove) / number_of_files * 100)}%. Removing them now.")
+    print(f"Percentage of outliers {len(to_remove) / number_of_files * 100}%. Removing them now.")
 
     for folder in folders:
         remove_outliers(folder, to_remove)
@@ -177,54 +198,205 @@ def remove_outliers(folder: Path, files_to_remove: list[str]):
         assert file_location.exists(), file_location
         file_location.unlink()
 
-# def check_means():
-#     folder_names = ["4b", "8b", "10m", "20m"]
-#     folders = [DATA_DIR.joinpath("processed", folder_name) for folder_name in folder_names]
-
-#     max_diff_sentinel = 0
-#     max_diff_planet = 0
-#     for file in folders[0].iterdir():
-#         try:
-#             np_4b = np.load(DATA_DIR.joinpath("processed", "4b", file.name)).mean()
-#             np_8b = np.load(DATA_DIR.joinpath("processed", "8b", file.name)).mean()
-#             np_10m = np.load(DATA_DIR.joinpath("processed", "10m", file.name)).mean()
-#             np_20m = np.load(DATA_DIR.joinpath("processed", "20m", file.name)).mean()
-#             thrsh = 1
-
-#             max_diff_planet = max(max_diff_planet, abs(np_4b - np_8b))
-#             max_diff_sentinel = max(max_diff_sentinel, abs(np_10m - np_20m))
-#             if abs(np_4b - np_8b) > thrsh:
-#                 print(file, "4b", "8b")
-
-#             if abs(np_10m - np_20m) > thrsh:
-#                 print(file, "10m", "20m")
-#         except:
-#             continue
-#     print(max_diff_planet, max_diff_sentinel)
-
 def do_histogram_matching(s2_bands: str, ps_bands: str):
     s2_folder = DATA_DIR.joinpath("processed", s2_bands)
     ps_folder = DATA_DIR.joinpath("processed", ps_bands)
+    histogram_folder = DATA_DIR.joinpath("processed", f"hist_{s2_bands}_{ps_bands}")
+    histogram_folder.mkdir()
 
     filenames = [file.name for file in s2_folder.iterdir()]
     for filename in filenames:
         s2_file = s2_folder.joinpath(filename)
         ps_file = ps_folder.joinpath(filename)
-        np.save(ps_file, match_histograms(np.load(ps_file), np.load(s2_file)))
+        np.save(histogram_folder.joinpath(filename), match_histograms(np.load(ps_file), np.load(s2_file)))
+
+def psnr(x, y):
+    return 20 * log10(8. / sqrt(np.mean((x - y) ** 2)))
+
+def plot_histogram(s2_bands: str, ps_bands: str):
+    s2_folder = DATA_DIR.joinpath("processed", s2_bands)
+    ps_folder = DATA_DIR.joinpath("processed", ps_bands)
+    hist_folder = DATA_DIR.joinpath("processed", f"hist_{s2_bands}_{ps_bands}")
+
+    filenames = [file.name for file in s2_folder.iterdir()]
+    assert filenames == [file.name for file in ps_folder.iterdir()]
+    assert filenames == [file.name for file in hist_folder.iterdir()]
+
+    scores_normal_ssim = []
+    scores_normal_psnr = []
+    scores_hist_ssim = []
+    scores_hist_psnr = []
+
+    for filename in filenames:
+        s2_file = np.load(s2_folder.joinpath(filename)) 
+        ps_file = np.load(ps_folder.joinpath(filename))
+        hist_file = np.load(hist_folder.joinpath(filename))
+
+        ssim_normal, _ = ssim((s2_file * (255 / 8)).astype(np.uint8), (ps_file * (255 / 8)).astype(np.uint8), full=True)
+        ssim_hist, _ = ssim((s2_file * (255 / 8)).astype(np.uint8), (hist_file * (255 / 8)).astype(np.uint8), full=True)
+
+        psnr_normal = psnr(s2_file, ps_file)
+        psnr_hist = psnr(s2_file, hist_file)
+
+        scores_normal_ssim.append(ssim_normal)
+        scores_hist_ssim.append(ssim_hist)
+        scores_normal_psnr.append(psnr_normal)
+        scores_hist_psnr.append(psnr_hist)
+
+    plt.hist(scores_normal_psnr, bins=33, range=(0, 32), color='red', alpha=0.5, label='Normal PSNR')
+    plt.hist(scores_hist_psnr, bins=33, range=(0, 32), color='blue', alpha=0.5, label='HIST PSNR')
+    overlap = np.intersect1d(scores_normal_psnr, scores_hist_psnr)
+    plt.hist(overlap, bins=33, range=(0,32), color='purple', alpha=0.7, label='Overlap')
+    plt.legend()
+    plt.xlabel('PSNR')
+    plt.ylabel('Frequency')
+    plt.title(f'Histograms of PSNR values {s2_bands}-{ps_bands}')
+    plt.show()
+
+    plt.hist(scores_normal_ssim, bins=33, range=(0, 1), color='red', alpha=0.5, label='Normal SSIM')
+    plt.hist(scores_hist_ssim, bins=33, range=(0, 1), color='blue', alpha=0.5, label='HIST SSIM')
+    overlap = np.intersect1d(scores_normal_ssim, scores_hist_psnr)
+    plt.hist(overlap, bins=33, range=(0,1), color='purple', alpha=0.7, label='Overlap')
+    plt.legend()
+    plt.xlabel('SSIM')
+    plt.ylabel('Frequency')
+    plt.title(f'Histograms of SSIM values {s2_bands}-{ps_bands}')
+    plt.show()
+
+    print("Mean SSIM Normal: ", np.sum(scores_normal_ssim) / len(scores_normal_ssim))
+    print("Mean PSNR Normal: ", np.sum(scores_normal_psnr) / len(scores_normal_psnr))
+    print("Mean SSIM Hist: ", np.sum(scores_hist_ssim) / len(scores_hist_ssim))
+    print("Mean PSNR Hist: ", np.sum(scores_hist_psnr) / len(scores_hist_psnr))
+
+
+def test(s2_bands: str, ps_bands: str, hist: bool):
+    s2_folder = DATA_DIR.joinpath("processed", s2_bands)
+
+    if hist:
+        ps_folder = DATA_DIR.joinpath("processed", f"hist_{s2_bands}_{ps_bands}")
+        titel_appendix = "With histogram matching."
+    else:    
+        ps_folder = DATA_DIR.joinpath("processed", ps_bands)
+        titel_appendix = "No histogram matching."
+
+    filenames = [file.name for file in s2_folder.iterdir()]
+    scores = []
+    for filename in filenames:
+        s2_file = np.load(s2_folder.joinpath(filename)) 
+        ps_file = np.load(ps_folder.joinpath(filename))
+
+        ssim_score, _ = ssim((s2_file * (255 / 8)).astype(np.uint8), (ps_file * (255 / 8)).astype(np.uint8), full=True)
+        psnr_score = psnr(s2_file, ps_file)
+
+        scores.append((psnr_score, ssim_score, filename))
+
+    psnr_scores = [entry[0] for entry in scores]
+    print("PSNR Mean: ", np.sum(psnr_scores) / len(psnr_scores))
+
+    psnr_scores = sorted(scores, key=lambda x: x[0])
+    print(psnr_scores[-1])
+    biggest_psnr_s2 = np.load(s2_folder.joinpath(psnr_scores[-1][2]))
+    biggest_psnr_s2 = np.load(ps_folder.joinpath(psnr_scores[-1][2]))
+    plt.title(f"Biggest PSNR score {psnr_scores[-1][0]}")
+    plt.imshow(np.concatenate((biggest_psnr_s2, biggest_psnr_s2), axis=1))
+    plt.show()
+
+    print(psnr_scores[0])
+    lowest_psnr_s2 = np.load(s2_folder.joinpath(psnr_scores[0][2]))
+    lowest_psnr_ps = np.load(ps_folder.joinpath(psnr_scores[0][2]))
+    plt.title(f"Lowest PSNR score {psnr_scores[0][0]}")
+    plt.imshow(np.concatenate((lowest_psnr_s2, lowest_psnr_ps), axis=1))
+    plt.show()
+
+    ssim_scores = sorted(scores, key=lambda x: x[1])
+    print(ssim_scores[-1])
+    biggest_similar_s2 = np.load(s2_folder.joinpath(ssim_scores[-1][2]))
+    biggest_similar_ps = np.load(ps_folder.joinpath(ssim_scores[-1][2]))
+    plt.title(f"Biggest similarity score {ssim_scores[-1][1]}")
+    plt.imshow(np.concatenate((biggest_similar_s2, biggest_similar_ps), axis=1))
+    plt.show()
+
+    print(ssim_scores[0])
+    lowest_similar_s2 = np.load(s2_folder.joinpath(ssim_scores[0][2]))
+    lowest_similar_ps = np.load(ps_folder.joinpath(ssim_scores[0][2]))
+    plt.title(f"Lowest similarity score {ssim_scores[0][1]}")
+    plt.imshow(np.concatenate((lowest_similar_s2, lowest_similar_ps), axis=1))
+    plt.show()
+
+def show_error():
+    filename = "2022_07_0263_03.npy"
+    ps_file = DATA_DIR.joinpath("processed", "4b", filename)
+    s2_file = DATA_DIR.joinpath("processed", "10m", filename)
+
+    ps_np = np.load(ps_file)
+    s2_np = np.load(s2_file)
+
+    diff = np.abs(ps_np - s2_np)
+
+    x = np.concatenate((s2_np, diff, ps_np), axis=1)
+
+    plt.title(f"Biggest difference")
+    plt.imshow(x)
+    plt.show()
+
+def remove_lowest_10_percentile(s2_bands: str, ps_bands: str, hist: bool) -> None:
+    s2_folder = DATA_DIR.joinpath("processed", s2_bands)
+    
+    if hist is True:
+        ps_folder = DATA_DIR.joinpath("processed", f"hist_{s2_bands}_{ps_bands}") 
+    else:
+        ps_folder = DATA_DIR.joinpath("processed", ps_bands)
+
+    filenames = [file.name for file in s2_folder.iterdir()]
+    assert filenames == [file.name for file in ps_folder.iterdir()]
+
+    scores_ssim = []
+    scores_psnr = []
+
+    for filename in filenames:
+        s2_file = np.load(s2_folder.joinpath(filename)) 
+        ps_file = np.load(ps_folder.joinpath(filename))
+
+        ssim_normal, _ = ssim((s2_file * (255 / 8)).astype(np.uint8), (ps_file * (255 / 8)).astype(np.uint8), full=True)
+        psnr_normal = psnr(s2_file, ps_file)
+
+        scores_ssim.append((ssim_normal, filename))
+        scores_psnr.append((psnr_normal, filename))
+
+    sorted_ssim = [x for x in sorted(scores_ssim, key = lambda x: x[0])]
+    sorted_psnr = [x for x in sorted(scores_psnr, key = lambda x: x[0])]
+
+    first_10_percent_ssim = sorted_ssim[0:int(len(sorted_ssim)*0.1)]
+    first_10_percent_psnr = sorted_psnr[0:int(len(sorted_psnr)*0.1)]
 
 
 def main():
-    preprocess(satellite="sentinel", in_situ=False)
-    preprocess(satellite="planetscope", in_situ=False)
-    remove_unused_images(in_situ=False)
+    # preprocess(satellite="sentinel", in_situ=False)
+    # preprocess(satellite="planetscope", in_situ=False)
+    # remove_unused_images(in_situ=False)
 
-    preprocess(satellite="sentinel", in_situ=True)
-    preprocess(satellite="planetscope", in_situ=True)
-    remove_unused_images(in_situ=True)
-    rename_in_situ_data()
+    # preprocess(satellite="sentinel", in_situ=True)
+    # preprocess(satellite="planetscope", in_situ=True)
+    # remove_unused_images(in_situ=True)
+    # rename_in_situ_data()
 
-    check_and_remove_outliers()
-    do_histogram_matching("10m", "4b")
-    
+    # do_histogram_matching("10m", "4b")
+    # do_histogram_matching("10m", "8b")
+    # do_histogram_matching("20m", "4b")
+    # do_histogram_matching("20m", "8b")
+
+    remove_lowest_10_percentile("20m", "4b", hist=False)
+    # test("10m", "4b", False)
+    # test("10m", "8b", False)
+    # test("20m", "4b", False)
+    # test("20m", "8b", False)
+
+    # plot_histogram("10m", "4b")
+    # plot_histogram("10m", "8b")
+    # plot_histogram("20m", "4b")
+    # plot_histogram("20m", "8b")
+
+    # show_error()
+
 if __name__ == "__main__":
     main()
