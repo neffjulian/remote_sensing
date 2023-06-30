546123375f488cf0d8e9151f094c9fdc1e038d75
neffjulian <julian.neff@inf.ethz.ch>
2023-06-30 15:28:08 +0200

    test

--- src/model/srcnn.py
+++ src/model/srcnn.py
@@ -50,6 +50,7 @@ class SRCNN(LightningModule):
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        return optimizer
         return {
             'optimizer': optimizer,
             'lr_scheduler': {
