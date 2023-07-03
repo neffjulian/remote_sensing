94b630f9c8af89bd2247b3b05881f0b69204eb66
neffjulian <julian.neff@inf.ethz.ch>
2023-07-03 15:31:12 +0200

    Added planetscope download

--- src/download_data.py
+++ src/download_data.py
@@ -8,22 +8,27 @@ SENTINEL_IN_SITU = DATA_DIR.joinpath("filtered", "in_situ")
 
 def download_zip_files():
     url = "https://polybox.ethz.ch/index.php/s/e5yrmHcejuCo0WO/download"
-    sentinel_in_situ_url = "https://polybox.ethz.ch/index.php/s/Wq2qZ4ZOw9K9Qyy/download"
+    # sentinel_in_situ_url = "https://polybox.ethz.ch/index.php/s/Wq2qZ4ZOw9K9Qyy/download"
+    planetscope_in_situ_url = "https://polybox.ethz.ch/index.php/s/d2wdH4nh37fP2Pj/download"
     # wget.download(url,out=str(DATA_DIR))
-    wget.download(sentinel_in_situ_url,out=str(SENTINEL_IN_SITU))
+    # wget.download(sentinel_in_situ_url,out=str(SENTINEL_IN_SITU))
+    wget.download(planetscope_in_situ_url,out=str(SENTINEL_IN_SITU))
 
 def unzip_and_delete():
     data_zip = DATA_DIR.joinpath("filtered.7z")
-    sentinel_in_situ_zip = SENTINEL_IN_SITU.joinpath("sentinel_in_situ.7z")
-
+    # sentinel_in_situ_zip = SENTINEL_IN_SITU.joinpath("sentinel_in_situ.7z")
+    planetscope_in_situ_zip = SENTINEL_IN_SITU.joinpath("planetscope_in_situ.7z")
     # with py7zr.SevenZipFile(data_zip) as z:
     #     z.extractall(DATA_DIR)
 
-    with py7zr.SevenZipFile(sentinel_in_situ_zip) as z:
+    # with py7zr.SevenZipFile(sentinel_in_situ_zip) as z:
+    #     z.extractall(SENTINEL_IN_SITU)
+
+    with py7zr.SevenZipFile(planetscope_in_situ_zip) as z:
         z.extractall(SENTINEL_IN_SITU)
 
     sentinel_in_situ_zip.unlink()
-
+    planetscope_in_situ_zip.unlink()
     # data_zip.unlink()
 
 if __name__ == "__main__":
