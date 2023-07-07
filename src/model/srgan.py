9aada4b198f1409cad3e1cc88220f159abfcbd8c
neffjulian <julian.neff@inf.ethz.ch>
2023-07-07 13:51:25 +0200

    Test

--- src/model/srgan.py
+++ src/model/srgan.py
@@ -214,4 +214,6 @@ class SRGAN(pl.LightningModule):
     def _perceptual_loss(self, hr_image: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
         real_features = self.feature_extractor(hr_image)
         fake_features = self.feature_extractor(fake)
+        print(hr_image.shape, fake.shape)
+        print(real_features.shape, fake_features.shape)
         return F.mse_loss(fake_features, real_features)
