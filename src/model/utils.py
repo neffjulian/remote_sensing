20f1e08e25881e39334c85c002835cb3def5c900
neffjulian <julian.neff@inf.ethz.ch>
2023-07-12 16:58:26 +0200

    Added changes to EDSR

--- src/model/utils.py
+++ src/model/utils.py
@@ -138,7 +138,7 @@ def visualize_output(name: str, output: list) -> None:
         sr_hr_ssim, _ = ssim((out[1] * (255.0 / 8.0)).astype(np.uint8), (out[2] * (255.0 / 8.0)).astype(np.uint8), full=True)
         print(out_file.name, "LR-HR PSNR:", lr_hr_psnr, "  SR-HR PSNR:", sr_hr_psnr," |  LR-HR SSIM:", lr_hr_ssim, " SR-HR SSIM:", sr_hr_ssim)
         j += 1
-        if not out[3].endswith("error"):
+        if not out[3].endswith("error") and "_" in out[3]:
             if (sr_hr_ssim > 0.5 or sr_hr_psnr > 25) and j < 5:
                 save_output_visualization(out[0], out[1], out[2], out_file)
                 np.save(results.joinpath(out[3] + '.npy'), out[2])
