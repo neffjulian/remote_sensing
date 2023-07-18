40fa0320d5db8df6c0cc702a5b3a30593f0695bc
neffjulian <julian.neff@inf.ethz.ch>
2023-07-18 11:39:57 +0200

    added lr encoder

--- src/model/rrdb.py
+++ src/model/rrdb.py
@@ -85,7 +85,7 @@ class RRDB(pl.LightningModule):
         return self.model(x)
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         return {
             'optimizer': optimizer,
             'lr_scheduler': {
