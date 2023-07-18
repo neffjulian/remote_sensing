7a3b19f9163658f87b3688610c481a21898bc454
neffjulian <julian.neff@inf.ethz.ch>
2023-07-18 09:13:38 +0200

    added lr encoder

--- src/model/rrdb.py
+++ src/model/rrdb.py
@@ -61,11 +61,17 @@ class RRDB(LightningModule):
             nn.Conv2d(self.channels, 1, kernel_size=3),
         )
 
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                torch.nn.init.normal_(module.weight, mean=0, std=0.0001)
+                if module.bias is not None:
+                    module.bias.data.zero_()
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.out(self.blocks(self.upsample(x)))
     
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=0.9)
         return {
             'optimizer': optimizer,
             'lr_scheduler': {
