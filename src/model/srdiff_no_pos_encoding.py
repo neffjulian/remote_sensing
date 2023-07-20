574dc216f213f677f95e0cec40133e9827c37e57
neffjulian <julian.neff@inf.ethz.ch>
2023-07-20 11:51:32 +0200

    Test: SRDIFF

--- src/model/srdiff_no_pos_encoding.py
+++ src/model/srdiff_no_pos_encoding.py
@@ -258,7 +258,7 @@ class SRDIFF_simple(LightningModule):
         x_r = x_H - up_x_L
 
         num_imgs = x_L.shape[0]
-        ts = torch.randint(0, self.T, size=(num_imgs,))
+        ts = torch.randint(0, self.T, size=(num_imgs,), device=self.device)
         alpha_hat_ts = self.alpha_hat[ts]
         noise = torch.normal(mean = 0, std = 1, size = x_H.shape, device=self.device)
         x_t = torch.sqrt(alpha_hat_ts).unsqueeze(1).unsqueeze(2).unsqueeze(3) * x_r + torch.sqrt(1. - alpha_hat_ts).unsqueeze(1).unsqueeze(2).unsqueeze(3) * noise
