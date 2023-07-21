5e145f8be1d8eb17ba8d8d37c49f0d61de2ef440
neffjulian <julian.neff@inf.ethz.ch>
2023-07-21 15:35:50 +0200

    Added test to create geotif files

--- src/test.py
+++ src/test.py
@@ -1,65 +1,53 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from geopy import distance
 from eodal.core.raster import RasterCollection
+from eodal.core.band import Band, GeoInfo
+import matplotlib.pyplot as plt
 
 s2_dir = Path(__file__).parent.parent.joinpath('data', 'processed', '20m_in_situ')
 ps_dir = Path(__file__).parent.parent.joinpath('data', 'processed', '4b_in_situ')
 
-orig_s2_dir = Path(__file__).parent.parent.joinpath('data', 'filtered', 'in_situ', 'sentinel_in_situ', '2022', '03_mar', 'lai', '0001_scene_20m_lai.tif')
-orig_ps_dir = Path(__file__).parent.parent.joinpath('data', 'filtered', 'in_situ', 'planetscope_in_situ', '2022', '03_mar', 'lai', '0001_lai_4bands.tif')
-
+results_dir = Path(__file__).parent.parent.joinpath('data', 'results')
 
 def main():
-    s2_file = np.load(s2_dir.joinpath("0001.npy"))
-    ps_file = np.load(ps_dir.joinpath("0001.npy"))
+    s2_tif_file = results_dir.joinpath('0001_scene_20m_lai.tif')
+    sr_npy_file = results_dir.joinpath('0001.npy')
+    ps_tif_file = results_dir.joinpath('0001_lai_4bands.tif')
 
-    s2_raster = RasterCollection.from_multi_band_raster(orig_s2_dir)
-    ps_raster = RasterCollection.from_multi_band_raster(orig_ps_dir)
+    s2_data = RasterCollection.from_multi_band_raster(s2_tif_file)
+    ps_data = RasterCollection.from_multi_band_raster(ps_tif_file)["lai"].values
+    sr_data = np.load(sr_npy_file)
 
-    x = s2_raster['lai'].values
+    band_name = "lai"
+    geo_info = s2_data["lai"].geo_info
+    epsg = geo_info.epsg
+    ulx = geo_info.ulx
+    uly = geo_info.uly
 
-    print(x.shape)
+    pixres_x, pixres_y = 3.33, -3.33
 
-    s2_raster['lai'].values = np.zeros(x.shape)
+    new_geo_info = GeoInfo(
+        epsg=epsg,
+        ulx=ulx,
+        uly=uly,
+        pixres_x=pixres_x,
+        pixres_y=pixres_y
+    )
 
-    print(s2_raster['lai'].values)
+    epsg = 32633
 
+    raster = RasterCollection(
+        band_constructor=Band,
+        band_name=band_name,
+        values = sr_data,
+        geo_info=new_geo_info
+    )
+
+    f = raster.plot_band("lai", colormap="viridis", vmin=0, vmax=8)
+    f.show()
+    plt.show()
 
 
 if __name__ == '__main__':
     main()
-
-
-    # Note: My indices start at 0 and the field ids start at 1.
-    # field_data = pd.read_csv(source_dir, sep=',')
-    # lai = field_data['lai'].to_numpy()
-
-    # error_s2 = []
-    # s2_lai = []
-    # indices = []
-    # for i, file in enumerate(s2_dir.iterdir()):
-    #     field_id = int(file.name[0:4]) + 1
-    #     data = np.load(file)
-    #     s2_lai.append(data[12, 12])
-    #     error_s2.append(abs(lai[field_id - 1] - data[12, 12]))
-    #     indices.append(int(file.name[0:4]) + 1)
-
-    # error_ps = []
-    # ps_lai = []
-    # for i, file in enumerate(ps_dir.iterdir()):
-    #     field_id = int(file.name[0:4]) + 1
-    #     data = np.load(file)
-    #     ps_lai.append((data[74, 75] + data[75, 74] + data[74, 74] + data[75, 75]) / 4)
-    #     error_ps.append(abs(lai[field_id - 1] - (data[74, 75] + data[75, 74] + data[74, 74] + data[75, 75]) / 4))
-
-    # lai_ = [lai[index - 1] for index in indices]
-
-    # for i in range(len(lai_)):
-    #     print(indices[i], lai_[i], s2_lai[i], ps_lai[i], error_s2[i], error_ps[i])
-
-    # df = pd.DataFrame({'field_id': indices, 'lai_in_situ': lai_, 'lai_est_s2': s2_lai, 'lai_est_ps': ps_lai, 'error_s2': error_s2, 'error_ps': error_ps})
-    # df.to_csv(tar_dir, index=False)
-
-    # print(df['error_s2'].mean(), df['error_ps'].mean())
